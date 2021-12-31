c2c169ac2957e2a88382eb06e0551177dbc98616
Xavier <xmtrinidad@gmail.com>
2021-12-30 19:27:27 -0700

    updated note positions

--- src/NOTE_POSITIONS.ts
+++ src/NOTE_POSITIONS.ts
@@ -3,15 +3,15 @@
 import { Note } from './interfaces/Note';
 
 const e_string: Note[] = [
-  {name: 'e', string: 1, fret: 0, position: [-10, 0]},
-  {name: 'f', string: 1, fret: 1, position: [23, 0]},
-  {name: 'g', string: 1, fret: 3, position: [176, 0]}
+  {name: 'E', string: 1, fret: 0, position: [-10, 0]},
+  {name: 'F', string: 1, fret: 1, position: [23, 0]},
+  {name: 'G', string: 1, fret: 3, position: [176, 0]}
 ];
 
 const b_string: Note[] = [
-  {name: 'b', string: 2, fret: 0, position: [-10, 29]},
-  {name: 'c', string: 2, fret: 1, position: [23, 29]},
-  {name: 'd', string: 2, fret: 3, position: [176, 29]}
+  {name: 'B', string: 2, fret: 0, position: [-10, 29]},
+  {name: 'C', string: 2, fret: 1, position: [23, 29]},
+  {name: 'D', string: 2, fret: 3, position: [176, 29]}
 ];
 
 export const NOTES: Note[] = [
